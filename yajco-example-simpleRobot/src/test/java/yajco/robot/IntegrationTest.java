a25aafb38ab8fd11e8a9feea98a99297cd83edff
Adrian Szegedi <adrian.szegedi@student.tuke.sk>
2020-08-09 18:51:32 +0200

    ## Simple Robot - fix test printer newline

--- yajco-example-simpleRobot/src/test/java/yajco/robot/IntegrationTest.java
+++ yajco-example-simpleRobot/src/test/java/yajco/robot/IntegrationTest.java
@@ -5,13 +5,13 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import yajco.robot.model.Robot;
-import yajco.robot.model.parser.ParseException;
 import yajco.robot.model.parser.Parser;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class IntegrationTest {
+	private static final String NL = System.lineSeparator();
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
 	@Before
@@ -27,19 +27,23 @@ public class IntegrationTest {
 	@Test
 	public void moveTest() throws Exception {
 		execute("begin\n\tmove\nend");
-		Assert.assertEquals("going straight\n", outContent.toString());
+		Assert.assertEquals("going straight" + NL, outContent.toString());
 	}
 
 	@Test
 	public void turnLeftTest() throws Exception {
 		execute("begin\n\tturn-left\nend");
-		Assert.assertEquals("turning left\n", outContent.toString());
+		Assert.assertEquals("turning left" + NL, outContent.toString());
 	}
 
 	@Test
 	public void executeProgramTest() throws Exception {
 		execute("begin\n\tturn-left\n\tmove\n\tmove\n\tturn-left\nend");
-		Assert.assertEquals("turning left\ngoing straight\ngoing straight\nturning left\n", outContent.toString());
+		final String expected = "turning left" + NL +
+				"going straight" + NL +
+				"going straight" + NL +
+				"turning left" + NL;
+		Assert.assertEquals(expected, outContent.toString());
 	}
 
 	private void execute(String input) throws Exception {
